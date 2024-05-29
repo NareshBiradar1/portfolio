e22cb10f3e1aae9cd45e6f45a024f30b376252b9
NareshBiradar1 <nareshdb555@gmail.com>
2024-05-29 12:54:59 +0530

    created navbar

---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+import './Navbar.css'
+function navBar(){
+    return (
+        <ul class="Navbar">
+            <li class="nav_list_item"><a href="">Home</a></li>
+            <li class="nav_list_item"><a href="">About</a></li>
+            <li class="nav_list_item"><a href="">Project</a></li>
+            <li class="nav_list_item"><a href="">Resume</a></li>    
+        </ul>
+    )
+}
+
+export default navBar;
---
+++ src/Typing.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+
+function Typing({
+    text,
+    typingSpeed = 100,
+    deletingSpeed = 50
+}) {
+    const [displayedText, setDisplayedText] = useState("");
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        // Function to handle typing and deleting logic
+        const handleTyping = () => {
+            if (!isDeleting) {
+                if (displayedText.length < text[index].length) {
+                    setDisplayedText(prev => prev + text[index].charAt(displayedText.length));
+                } else {
+                    setIsDeleting(true);
+                }
+            } else {
+                if (displayedText.length > 0) {
+                    setDisplayedText(prev => prev.slice(0, -1));
+                } else {
+                    setIsDeleting(false);
+                    setIndex(prev => (prev + 1) % text.length);
+                }
+            }
+        };
+
+        const timeout = setTimeout(handleTyping, isDeleting ? deletingSpeed : typingSpeed);
+
+        return () => clearTimeout(timeout);
+    }, [displayedText, isDeleting, index, text, typingSpeed, deletingSpeed]);
+
+    return (
+        <div className="typing-effect">
+            <p>{displayedText}</p>
+        </div>
+    );
+}
+
+export default Typing;
