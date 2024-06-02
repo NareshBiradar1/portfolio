e56ecb1d58a4e2d57b88b4974225ac1003d14cfb
NareshBiradar1 <nareshdb555@gmail.com>
2024-06-02 18:53:21 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/NavBar';
 import SkillsPage from './Components/Skills/SkillPage';
 import Home from'./Components/HomePage/Home';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <><NavBar/><Home/></>
+  },
+  {
+    path: "/skills",
+    element: <><NavBar/><SkillsPage /></>
+  }
+])
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <NavBar/>
-      </header>
-      <Home/>
-      <SkillsPage/>
-    </div>
+    <>
+      
+      <RouterProvider router={router} />
+
+    </>
   );
 }
 
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,12 +1,21 @@
 import './Navbar.css'
+import logo from '../assets/logo.svg';
 function navBar(){
     return (
-        <ul class="Navbar">
-            <li class="nav_list_item"><a href="">Home</a></li>
-            <li class="nav_list_item"><a href="">About</a></li>
-            <li class="nav_list_item"><a href="">Skills</a></li>
-            <li class="nav_list_item"><a href="">Project</a></li>    
-        </ul>
+        <>
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <ul class="Navbar">
+                    <li class="nav_list_item"><a href="/">Home</a></li>
+                    <li class="nav_list_item"><a href="">About</a></li>
+                    <li class="nav_list_item"><a href="/skills">Skills</a></li>
+                    <li class="nav_list_item"><a href="">Project</a></li>    
+                </ul>   
+            </header>
+            
+        </div>
+        </>     
     )
 }
 
